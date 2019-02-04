9f7c071016792147002f5317e16e31cc4a6d79b5
kimromi <kimromi4@gmail.com>
2019-02-05 01:28:08 +0900

    rubocop -a

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 RunningStation::Engine.routes.draw do
-  root to: 'station#index'
+  root to: "station#index"
 
   resources :runners, param: :name, only: %i(show update)
 end
