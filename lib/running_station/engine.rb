d83807ef8264b1fa0764c594950fa572e42e3a09
kimromi <kimromi4@gmail.com>
2019-02-05 00:35:04 +0900

    require all dependencies for rails engine

--- lib/running_station/engine.rb
+++ lib/running_station/engine.rb
@@ -3,5 +3,10 @@
 module RunningStation
   class Engine < ::Rails::Engine
     isolate_namespace RunningStation
+
+    # require all dependencies
+    Gem.loaded_specs['running_station'].dependencies.each do |d|
+      require d.name if d.type == :runtime
+    end
   end
 end
