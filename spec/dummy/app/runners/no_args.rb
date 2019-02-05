9a650594965c5f4f752e4b1001a9fbb371dcc203
kimromi <kimromi4@gmail.com>
2019-02-06 01:03:46 +0900

    design

--- spec/dummy/app/runners/no_args.rb
+++ spec/dummy/app/runners/no_args.rb
@@ -1,3 +1,38 @@
 # frozen_string_literal: true
 
-print "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+puts "Hello! Running Station"
+
+class Test
+  class << self
+    def aaa
+      puts "bbb"
+    end
+  end
+end
+Test.aaa
